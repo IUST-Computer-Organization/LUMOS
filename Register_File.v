605bafe7467dd25adc7a7782c5c35794dccce166
Faraz <farazghoreishy@gmail.com>
2024-06-06 03:09:44 +0330

    Create Register_File.v

---
+++ Register_File.v
@@ -0,0 +1,62 @@
+//  LUMOS - Light Utilization with Multicycle Operational Stages
+//  A RISC-V RV32I Processor Core
+
+//  Description: LUMOS Core Register File Module
+//  Copyright 2024 Iran University of Science and Technology. <iustCompOrg@gmail.com>
+
+//  Permission to use, copy, modify, and/or distribute this software for any
+//  purpose with or without fee is hereby granted, provided that the above
+//  copyright notice and this permission notice appear in all copies.
+
+module Register_File
+#(
+    parameter WIDTH = 32,
+    parameter DEPTH = 5
+)
+(
+    input wire clk,
+    input wire reset,
+    
+    input wire read_enable_1,
+    input wire read_enable_2,
+    input wire write_enable,
+    
+    input wire [DEPTH - 1 : 0] read_index_1,
+    input wire [DEPTH - 1 : 0] read_index_2,
+    input wire [DEPTH - 1 : 0] write_index,
+
+    input wire [WIDTH - 1 : 0] write_data,
+
+    output reg [WIDTH - 1 : 0] read_data_1,
+    output reg [WIDTH - 1 : 0] read_data_2
+);
+	reg [WIDTH - 1 : 0] Registers [0 : 2 ** DEPTH - 1];      
+
+    integer i;    		
+    always @(posedge clk or posedge reset)
+    begin
+        if (reset)
+        begin
+            for (i = 0; i < 2 ** DEPTH; i = i + 1)
+                Registers[i] <= {WIDTH{1'b0}};
+        end
+        else
+        if (write_enable == 1'b1)
+        begin
+            Registers[write_index] <= write_data;
+        end
+    end
+
+    always @(*) 
+    begin
+        if (read_enable_1 == 1'b1 && read_index_1 != {DEPTH{1'bz}})
+            read_data_1 <= Registers[read_index_1];
+        else
+            read_data_1 <= {WIDTH{1'bz}};
+
+        if (read_enable_2 == 1'b1 && read_index_2 != {DEPTH{1'bz}})
+            read_data_2 <= Registers[read_index_2];
+        else
+            read_data_2 <= {WIDTH{1'bz}};
+    end
+endmodule
