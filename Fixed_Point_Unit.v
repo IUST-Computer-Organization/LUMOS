af67c84999701abff8e91646f73c279bbab63102
Faraz <farazghoreishy@gmail.com>
2024-06-06 17:58:56 +0330

    Create Fixed_Point_Unit.v

---
+++ Fixed_Point_Unit.v
@@ -0,0 +1,87 @@
+`include "Defines.vh"
+
+module Fixed_Point_Unit 
+#(
+    parameter WIDTH = 32,
+    parameter FBITS = 10
+)
+(
+    input wire clk,
+    input wire reset,
+    
+    input wire [WIDTH - 1 : 0] operand_1,
+    input wire [WIDTH - 1 : 0] operand_2,
+    
+    input wire [ 1 : 0] operation,
+
+    output reg [WIDTH - 1 : 0] result,
+    output reg ready
+);
+
+    always @(*)
+    begin
+        case (operation)
+            `FPU_ADD    : begin result <= operand_1 + operand_2; ready <= 1; end
+            `FPU_SUB    : begin result <= operand_1 - operand_2; ready <= 1; end
+            `FPU_MUL    : begin result <= product[WIDTH + FBITS - 1 : FBITS]; ready <= product_ready; end
+            `FPU_SQRT   : begin result <= root; ready <= root_ready; end
+            default     : begin result <= 'bz; ready <= 0; end
+        endcase
+    end
+
+    always @(posedge reset)
+    begin
+        if (reset)  ready = 0;
+        else        ready = 'bz;
+    end
+    // ------------------- //
+    // Square Root Circuit //
+    // ------------------- //
+    reg [WIDTH - 1 : 0] root;
+    reg root_ready;
+
+        /*
+         *  Describe Your Square Root Calculator Circuit Here.
+         */
+
+    // ------------------ //
+    // Multiplier Circuit //
+    // ------------------ //   
+    reg [64 - 1 : 0] product;
+    reg product_ready;
+
+    reg     [15 : 0] multiplierCircuitInput1;
+    reg     [15 : 0] multiplierCircuitInput2;
+    wire    [31 : 0] multiplierCircuitResult;
+
+    Multiplier multiplier_circuit
+    (
+        .operand_1(multiplierCircuitInput1),
+        .operand_2(multiplierCircuitInput2),
+        .product(multiplierCircuitResult)
+    );
+
+    reg     [31 : 0] partialProduct1;
+    reg     [31 : 0] partialProduct2;
+    reg     [31 : 0] partialProduct3;
+    reg     [31 : 0] partialProduct4;
+
+        /*
+         *  Describe Your 32-bit Multiplier Circuit Here.
+         */
+         
+endmodule
+
+module Multiplier
+(
+    input wire [15 : 0] operand_1,
+    input wire [15 : 0] operand_2,
+
+    output reg [31 : 0] product
+);
+
+    always @(*)
+    begin
+        product <= operand_1 * operand_2;
+    end
+endmodule
